0690e625d383d766d36397643df8048a67b09b22
Ben Hayward <bhbenhayward@gmail.com>
2020-04-10 00:38:20 +0100

    Create Project form fully validated
    Dates are validated, however they need further improvement.

--- Models/ViewModels/ProjectCreateViewModel.cs
+++ Models/ViewModels/ProjectCreateViewModel.cs
@@ -23,9 +23,6 @@ namespace SEPMTool.Models.ViewModels
         //[Required(ErrorMessage = "Project requirements are required")]
         public List<ProjectRequirement> ProjectRequirements { get; set; }
         public Priority Priority { get; set; }
-        public Status Status { get; set; }
-        public bool AwardEligibility { get; set; }
-        public decimal Progress { get; set; }
         public List<ProjectUsersViewModel> Users { get; set; }
         [Required(ErrorMessage = "Project start date is required")]
         public DateTime StartDate { get; set; }
@@ -33,15 +30,6 @@ namespace SEPMTool.Models.ViewModels
         public DateTime Deadline { get; set; }
         public DateTime EstimatedCompletionDate { get; set; }
         public List<ProjectUsersViewModel> AllUsers { get; set; }
-        public ProjectRequirement AddRequirement { get; set; }
-        public string RequirementName { get; set; }
-        public string RequirementDescription { get; set; }
-        public Priority RequirementPriority { get; set; }
-        public ProjectRequirement ProjectRequirement { get; set; }
-        public RequirementCategory RequirementCategory { get; set; }
         public int RequirementIndex { get; set; }
-        public bool ShowRequirements { get; set; }
-        public bool ShowModal { get; set; }
-        public ProjectRequirement NewRequirement { get; set; }
     }
 }
